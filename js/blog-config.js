71f3857478724cbaf67e36719ebb71995f77b645
Reonica <148539983+reonica@users.noreply.github.com>
2025-08-21 22:59:57 +0700

    Create blog-config.js

---
+++ js/blog-config.js
@@ -0,0 +1,18 @@
+// Blog Configuration
+window.BLOG_CONFIG = {
+    github: {
+        owner: 'reonica',
+        repo: 'alda',
+        token: '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',
+        blogPath: 'blog',
+        branch: 'main'
+    },
+    
+    site: {
+        name: 'Alda Hub',
+        url: 'https://aldahub.com',
+        defaultAuthor: 'Alda Hub Team'
+    },
+    
+    postsPerPage: 10
+};
