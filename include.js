58a1245a7d28734b9b0fafe171f0c267833e32cd
Reonica <148539983+reonica@users.noreply.github.com>
2025-10-07 14:34:47 +0700

    Update include.js

--- include.js
+++ include.js
@@ -2,7 +2,7 @@ function includeHTML() {
   const elements = document.querySelectorAll('[data-include]');
   elements.forEach(el => {
     const file = el.getAttribute('data-include');
-    fetch(`_includes/${file}`)
+    fetch(`includes/${file}`)
       .then(response => {
         if (!response.ok) throw new Error(`Could not load ${file}`);
         return response.text();
