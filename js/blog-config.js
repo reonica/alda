872bacbe5e7b099592aa3855661c00d2c7f61332
Reonica <148539983+reonica@users.noreply.github.com>
2026-01-01 23:13:26 +0700

    Update blog-config.js

--- js/blog-config.js
+++ js/blog-config.js
@@ -1,18 +1,18 @@
 // Blog Configuration
 window.BLOG_CONFIG = {
-    github: {
-        owner: 'reonica',
-        repo: 'alda',
-        token: '',
-        blogPath: 'blog',
-        branch: 'main'
-    },
-    
-    site: {
-        name: 'Alda Hub',
-        url: 'https://aldahub.com',
-        defaultAuthor: 'Alda Hub Team'
-    },
-    
-    postsPerPage: 6
+  github: {
+    owner: 'reonica',
+    repo: 'alda',
+    token: '',
+    blogPath: 'blog',
+    branch: 'main'
+  },
+  site: {
+    name: 'Alda Hub',
+    url: 'https://aldahub.com',
+    defaultAuthor: 'Alda Hub Team'
+  },
+  postsPerPage: 6,
+  apiUrl: 'https://api.example.com/api/posts',
+  perPage: 10
 };
